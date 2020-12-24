0f47ee3aca200be8a42f64d6b93becd0881f2c5b
erichevers <erichevers@gmail.com>
2020-12-24 20:13:58 +0100

    Bit more logging to see where things go wrong

--- src/deepstream_fr.py
+++ src/deepstream_fr.py
@@ -26,6 +26,7 @@ import cv2
 import os
 import os.path
 from os import path
+import logging
 import face_recognition  # to load the face recognition libary
 import pickle            # to load the trained faces file
 from pathlib import Path  # to auto create directories
@@ -59,8 +60,8 @@ logfile = logdir + process + '.log'
 loglevel = 'Info'
 logsize = 200000
 logbackups = 5
-global log
-log = None
+log = logging.NOTSET
+
 
 # other variables
 learnedfile = logdir + 'known_faces/trained_faces.pkl'
@@ -69,7 +70,8 @@ gui = True  # use the GUI as output to show the stream
 save_unknown = True  # save unknow faces
 unknown_face_name = 'Unknown'  # what name to use when the face is not recognized
 sampling_rate = 20   # process every X frames in a stream i.e. sampling_rate of 2 will process every other frame, 4 will 1 frame of 4 etc
-resize_factor = 2   # to resize the video stream, set to 4 for high quality streams (> 1920x1080:24fps) to shrink the resolution for better performance
+# TODO: resize factor should be 2
+resize_factor = 1   # to resize the video stream, set to 4 for high quality streams (> 1920x1080:24fps) to shrink the resolution for better performance
 up_scale = 2     # This finds faces better when they are small (1 = standard, 3 & 4 is slow)
 detection_model = 'hog'  # this is the model that is used for face recognition: models can be "hog", "cnn". cnn is more accurate, but takes longer
 number_jitters = 1             # How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
@@ -78,15 +80,11 @@ capture_filename = 'Frame-'  # this is the name to be used when a screenshot is 
 unknown_face_filename = 'UnknownFace-'  # this is the name to be used when an unknow face is found
 border = 50  # store unknown face with some surrounding
 transcoder = cv2.CAP_FFMPEG  # cv2.CAP_V4L cv2.CAP_V4L2 cv2.CAP_DC1394 cv2.CAP_GSTREAMER or cv2.CAP_FFMPEG
-# additional global variables
-global Names
+# additional variables
 Names = []
-global Sequence
 Sequence = []
-global Encodings
 Encodings = []
-global folder_name
-folder_name = None
+folder_name = logdir + '/frames'
 
 
 # tiler_sink_pad_buffer_probe  will extract metadata received on tiler src pad
@@ -137,6 +135,7 @@ def tiler_sink_pad_buffer_probe(pad, info, u_data):
             # Periodically check for objects with borderline confidence value that may be false positive detections.
             # If such detections are found, annoate the frame with bboxes and confidence value.
             # Save the annotated frame to file.
+            # The below proces will be rewritten later, but we keep it in for reference
             if((saved_count["stream_" + str(frame_meta.pad_index)] % 30 == 0) and (obj_meta.confidence > 0.3 and obj_meta.confidence < 0.31)):
                 if is_first_obj:
                     is_first_obj = False
@@ -148,24 +147,24 @@ def tiler_sink_pad_buffer_probe(pad, info, u_data):
                     # covert the array into cv2 default color format
                     frame_image = cv2.cvtColor(frame_image, cv2.COLOR_RGBA2BGRA)
 
-                save_image = True
-                frame_image = draw_bounding_boxes(frame_image, obj_meta, obj_meta.confidence)
+                # save_image = True  # TODO: Don't save images for now
+                frame_image = add_confidence_box(frame_image, obj_meta, obj_meta.confidence)
             # save and draw box when we have a person
-            if obj_meta.class_id == PGIE_CLASS_ID_PERSON and frame_meta.pad_index % sampling_rate == 0:
-                # only do face recognition if the object is a person and once in a while depending on the sampling rate
+            if frame_meta.pad_index % sampling_rate == 0:
+                # only draw a box once in a while depending on the sampling rate
                 # Getting Image data using nvbufsurface
                 # the input should be address of buffer and batch_id
                 n_frame = pyds.get_nvds_buf_surface(hash(gst_buffer), frame_meta.batch_id)
                 # convert python array into numpy array format.
                 frame_image = np.array(n_frame, copy=True, order='C')
-                frame_image = draw_bounding_boxes(frame_image, obj_meta, obj_meta.confidence)
+                frame_image = draw_bounding_boxes(frame_image, obj_meta)
             # continue with the next object when there is one
             try:
                 l_obj = l_obj.next
             except StopIteration:
                 break
 
-        print("Frame Number=", frame_number, "Number of Objects=", num_rects, "Vehicle_count=", obj_counter[PGIE_CLASS_ID_VEHICLE], "Person_count=", obj_counter[PGIE_CLASS_ID_PERSON])
+        log.info(f'Frame Number: {frame_number}, Number of Objects: {num_rects}, Vehicle_count: {obj_counter[PGIE_CLASS_ID_VEHICLE]}, Person_count={obj_counter[PGIE_CLASS_ID_PERSON]}')
         # Get frame rate through this probe
         fps_streams["stream{0}".format(frame_meta.pad_index)].get_fps()
         if save_image:
@@ -185,6 +184,7 @@ def face_recog(image, obj_meta):
     obj_left = int(obj_coordinates.left)
     obj_bottom = obj_top - int(obj_coordinates.height)
     obj_right = obj_left + int(obj_coordinates.width)
+    log.info(f'--- Object located at location top: {obj_top}, left: {obj_left}, bottom: {obj_bottom}, right: {obj_right}')
     # Resize frame of video for faster face recognition processing, when required
     if resize_factor == 1:
         small_frame = image[obj_top - border:obj_bottom + border, obj_left - border:obj_right + border]  # only take the object part from the image for faster recognition
@@ -218,7 +218,7 @@ def face_recog(image, obj_meta):
 
     # display the results
     if face_names:
-        frame = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # the image coming from the pipeline is RGB, and OpenCV is in BGR so we need to convert the image
+        frame = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)  # the image coming from the pipeline is RGBA, and OpenCV is in BGR so we need to convert the image
         for (top, right, bottom, left), face_name in zip(face_locations, face_names):
             if resize_factor != 1:
                 top *= resize_factor
@@ -237,16 +237,30 @@ def face_recog(image, obj_meta):
                     j += 1
                 cv2.imwrite(f'{unknow_face_dir}{unknown_face_filename}{j}.jpg', frame[top - border:bottom + border, left - border:right + border])  # only save the face with a border -> crop image using numphy
             # draw a box around the face
+            log.info(f'-- Adding box for: {face_name}, at location top: {top}, left: {left}, bottom: {bottom}, right: {right}')
             cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 0), 2)
             # draw a box above the face
             cv2.rectangle(frame, (left, top - 25), (left + 200, top), (0, 255, 255), -1)
             cv2.putText(frame, face_name, (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, .75, (255, 0, 0), 2)
-            # write updated image back in the pipeline format (RBGA) when there are any faces and at the location on the image, not the cropped object
-        image = cv2.cvtColor(frame, cv2.COLOR_BGRA2RBGA)
+        # write updated image back in the pipeline format (RGBA) when there are any faces and at the location on the image, not the cropped object
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     return image
 
 
-def draw_bounding_boxes(image, obj_meta, confidence):
+def draw_bounding_boxes(image, obj_meta):
+    if obj_meta.class_id == PGIE_CLASS_ID_PERSON:
+        # this is a person, let's see if we know this person and draw a box around the face
+        image = face_recog(image, obj_meta)
+    elif obj_meta.class_id == PGIE_CLASS_ID_VEHICLE:
+        # this is a vehicle, let's see if we can find a license plate
+        log.info('-- Vehicle detected, but no function yet to check his license plate')
+    else:
+        # some other object, do nothing
+        log.info('-- Object detected, but it is not a person or a vehicle')
+    return image
+
+
+def add_confidence_box(image, obj_meta, confidence):
     confidence = '{0:.2f}'.format(confidence)
     rect_params = obj_meta.rect_params
     top = int(rect_params.top)
@@ -257,15 +271,7 @@ def draw_bounding_boxes(image, obj_meta, confidence):
     image = cv2.rectangle(image, (left, top), (left + width, top + height), (0, 0, 255, 0), 2)  # TODO: i think the example is false and should be top - height. but let's see
     # Note that on some systems cv2.putText erroneously draws horizontal lines across the image
     image = cv2.putText(image, obj_name + ', C=' + str(confidence), (left - 10, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255, 0), 2)
-    if obj_meta.class_id == PGIE_CLASS_ID_PERSON:
-        # this is a person, let's see if we know this person and draw a box around the face
-        image = face_recog(image, obj_meta)
-    elif obj_meta.class_id == PGIE_CLASS_ID_VEHICLE:
-        # this is a vehicle, let's see if we can find a license plate
-        log.info('-- Vehicle detected, but no function yet to check his license plate')
-    else:
-        # some other object, do nothing
-        log.info('-- Object detected, but it is not a person or a vehicle')
+    log.info(f'-- Adding box for: {obj_name}, with confidence: {confidence} at location top: {top}, left: {left}, bottom: {top + height}, right: {left + width}')
     return image
 
 
@@ -348,12 +354,15 @@ def main(args):
         fps_streams["stream{0}".format(i)] = GETFPS(i)
     number_sources = len(args) - 2
 
+    global folder_name
     folder_name = args[-1]
-    if path.exists(folder_name):
-        sys.stderr.write("The output folder %s already exists. Please remove it first.\n" % folder_name)
-        sys.exit(1)
+    # TODO: check if this check is realy required
+    #  if path.exists(folder_name):
+    #    sys.stderr.write("The output folder %s already exists. Please remove it first.\n" % folder_name)
+    #    sys.exit(1)
 
     # start logging and counter and create directory for any unknow faces just in case we find any
+    global log
     logpath = Path(logdir)
     logpath.mkdir(parents=True, exist_ok=True)
     log = init_log(logfile, process, loglevel, logsize, logbackups)
@@ -366,93 +375,104 @@ def main(args):
     log.info(f'- Opening learned faces file: {learnedfile}')
     with open(learnedfile, 'rb') as trainedfacesfile:
         # reading the learned faces file
+        global Names
         Names = pickle.load(trainedfacesfile)
+        global Sequence
         Sequence = pickle.load(trainedfacesfile)
+        global Filedate
+        Filedate = pickle.load(trainedfacesfile)
+        global Encodings
         Encodings = pickle.load(trainedfacesfile)
 
-    os.mkdir(folder_name)
-    log.warning(f'- Frames will be saved in: {folder_name}')
+    # create directory to save ambigious objects
+    folder_path = Path(folder_name)
+    folder_path.mkdir(parents=True, exist_ok=True)
+    log.warning(f'- Ambigious objects will be saved in: {folder_path}')
+
     # Standard GStreamer initialization
     GObject.threads_init()
     Gst.init(None)
 
     # Create gstreamer elements */
     # Create Pipeline element that will form a connection of other elements
-    log.warning("- Creating Pipeline\n")
+    log.warning('- Creating Pipeline')
     pipeline = Gst.Pipeline()
     is_live = False
-
     if not pipeline:
-        sys.stderr.write(" Unable to create Pipeline \n")
-    print("Creating streamux \n ")
+        log.critical('Error: Unable to create Pipeline')
 
     # Create nvstreammux instance to form batches from one or more sources.
+    log.warning('- Creating streamux')
     streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
     if not streammux:
-        sys.stderr.write(" Unable to create NvStreamMux \n")
-
+        log.critical('Error: Unable to create NvStreamMux')
     pipeline.add(streammux)
     for i in range(number_sources):
         os.mkdir(folder_name + "/stream_" + str(i))
         frame_count["stream_" + str(i)] = 0
         saved_count["stream_" + str(i)] = 0
-        print("Creating source_bin ", i, " \n ")
+        log.info(f'Creating source_bin {i}')
         uri_name = args[i + 1]
         if uri_name.find("rtsp://") == 0:
             is_live = True
         source_bin = create_source_bin(i, uri_name)
         if not source_bin:
-            sys.stderr.write("Unable to create source bin \n")
+            log.critical('Error: Unable to create source bin')
         pipeline.add(source_bin)
         padname = "sink_%u" % i
         sinkpad = streammux.get_request_pad(padname)
         if not sinkpad:
-            sys.stderr.write("Unable to create sink pad bin \n")
+            log.critical('Unable to create sink pad bin')
         srcpad = source_bin.get_static_pad("src")
         if not srcpad:
-            sys.stderr.write("Unable to create src pad bin \n")
+            log.critical('Unable to create src pad bin')
         srcpad.link(sinkpad)
-    print("Creating Pgie \n ")
+
+    log.warning('- Creating Pgie')
     pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
     if not pgie:
-        sys.stderr.write(" Unable to create pgie \n")
+        log.critical('Error: Unable to create pgie')
+
     # Add nvvidconv1 and filter1 to convert the frames to RGBA
     # which is easier to work with in Python.
-    print("Creating nvvidconv1 \n ")
+    log.warning('Creating nvvidconv1 and filter1 to convert frames to RGBA')
     nvvidconv1 = Gst.ElementFactory.make("nvvideoconvert", "convertor1")
     if not nvvidconv1:
-        sys.stderr.write(" Unable to create nvvidconv1 \n")
-    print("Creating filter1 \n ")
+        log.critical('Error: Unable to create nvvidconv1')
     caps1 = Gst.Caps.from_string("video/x-raw(memory:NVMM), format=RGBA")
     filter1 = Gst.ElementFactory.make("capsfilter", "filter1")
     if not filter1:
-        sys.stderr.write(" Unable to get the caps filter1 \n")
+        log.critical('Error: Unable to get the caps filter1')
     filter1.set_property("caps", caps1)
-    print("Creating tiler \n ")
+
+    # creating tiler
+    log.warning('- Creating tiler')
     tiler = Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
     if not tiler:
-        sys.stderr.write(" Unable to create tiler \n")
-    print("Creating nvvidconv \n ")
+        log.critical('Error: Unable to create tiler')
+
+    log.warning('- Creating nvvidconv')
     nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
     if not nvvidconv:
-        sys.stderr.write(" Unable to create nvvidconv \n")
-    print("Creating nvosd \n ")
+        log.critical('Error: Unable to create nvvidconv')
+
+    log.warning('- Creating nvosd')
     nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
     if not nvosd:
-        sys.stderr.write(" Unable to create nvosd \n")
+        log.critical('Error: Unable to create nvosd')
     if(is_aarch64()):
-        print("Creating transform \n ")
+        log.warning('- Creating transform for arch64')
         transform = Gst.ElementFactory.make("nvegltransform", "nvegl-transform")
         if not transform:
-            sys.stderr.write(" Unable to create transform \n")
+            log.critical('Error: Unable to create transform')
 
-    print("Creating EGLSink \n")
+    log.warningp('- Creating EGLSink')
     sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
     if not sink:
-        sys.stderr.write(" Unable to create egl sink \n")
+        log.critical('Error: Unable to create egl sink')
 
     if is_live:
-        print("Atleast one of the sources is live")
+        log.info('- At least one of the sources is live')
         streammux.set_property('live-source', 1)
 
     streammux.set_property('width', 1920)
@@ -462,7 +482,7 @@ def main(args):
     pgie.set_property('config-file-path', "dstest_imagedata_config.txt")
     pgie_batch_size = pgie.get_property("batch-size")
     if(pgie_batch_size != number_sources):
-        print("WARNING: Overriding infer-config batch-size", pgie_batch_size, " with number of sources ", number_sources, " \n")
+        log.warning(f'Warning: Overriding infer-config batch-size {pgie_batch_size} with number of sources {number_sources}')
         pgie.set_property("batch-size", number_sources)
     tiler_rows = int(math.sqrt(number_sources))
     tiler_columns = int(math.ceil((1.0 * number_sources) / tiler_rows))
@@ -470,7 +490,6 @@ def main(args):
     tiler.set_property("columns", tiler_columns)
     tiler.set_property("width", TILED_OUTPUT_WIDTH)
     tiler.set_property("height", TILED_OUTPUT_HEIGHT)
-
     sink.set_property("sync", 0)
 
     if not is_aarch64():
@@ -482,7 +501,7 @@ def main(args):
         nvvidconv1.set_property("nvbuf-memory-type", mem_type)
         tiler.set_property("nvbuf-memory-type", mem_type)
 
-    print("Adding elements to Pipeline \n")
+    log.warning('- Adding elements to Pipeline')
     pipeline.add(pgie)
     pipeline.add(tiler)
     pipeline.add(nvvidconv)
@@ -493,7 +512,7 @@ def main(args):
         pipeline.add(transform)
     pipeline.add(sink)
 
-    print("Linking elements in the Pipeline \n")
+    log.warning('- Linking elements in the Pipeline')
     streammux.link(pgie)
     pgie.link(nvvidconv1)
     nvvidconv1.link(filter1)
@@ -507,6 +526,7 @@ def main(args):
         nvosd.link(sink)
 
     # create an event loop and feed gstreamer bus mesages to it
+    log.warning('- Create event loop')
     loop = GObject.MainLoop()
     bus = pipeline.get_bus()
     bus.add_signal_watch()
@@ -514,27 +534,27 @@ def main(args):
 
     tiler_sink_pad = tiler.get_static_pad("sink")
     if not tiler_sink_pad:
-        sys.stderr.write(" Unable to get src pad \n")
+        log.critical('Error: Unable to get src pad')
     else:
         tiler_sink_pad.add_probe(Gst.PadProbeType.BUFFER, tiler_sink_pad_buffer_probe, 0)
 
     # List the sources
-    log.info(f'- Now playing...')
+    log.info('- Now playing...')
     for i, source in enumerate(args[:-1]):
         if (i != 0):
             log.info(f'- {i}: {source}')
 
+    # start play back and listed to events
     log.info(f'- Starting pipeline and processing with sampling rate: {sampling_rate}, resize factor: {resize_factor} and up scale: {up_scale}')
     log.info(f'- Facial recognition is done with detection model: {detection_model}, number of jitters: {number_jitters} and encoding model: {encoding_model}')
-
-    # start play back and listed to events
     pipeline.set_state(Gst.State.PLAYING)
     try:
         loop.run()
     except:
         pass
     # cleanup
-    print("Exiting app\n")
+    endtime = time.perf_counter()
+    log.critical(f'Program {process} ended and took {endtime - starttime:0.2f} seconds to complete')
     pipeline.set_state(Gst.State.NULL)
 
 
