8442874d9362bd8ee7e4913ff92a804a9218559d
erichevers <erichevers@gmail.com>
2020-12-27 17:03:48 +0100

    remove working dir

--- DockerStart.sh
+++ DockerStart.sh
@@ -6,7 +6,7 @@ if [ $1 ]
 then
     xhost local:docker
     # use this when webcam is connected: --device /dev/video0:/dev/video0
-    sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -w /opt/nvidia/deepstream/deepstream-5.0 -v /tmp/.X11-unix/:/tmp/.X11-unix -v /home/eevers/nvme/projects/deepstream_fr/logdir:/opt/nvidia/deepstream/deepstream-5.0/sources/deepstream_python_apps/apps/deepstream_fr/logdir deepstream_fr:$1 
+    sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v /home/eevers/nvme/projects/deepstream_fr/logdir:/opt/nvidia/deepstream/deepstream-5.0/sources/deepstream_python_apps/apps/deepstream_fr/logdir deepstream_fr:$1 
 else
     echo "please specify imageversion"
 fi
